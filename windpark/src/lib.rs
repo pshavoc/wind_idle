ce43c60a00ee4749b36b60c94ce4daf31aba7136
Paul Soucy <paul.soucy@havocai.com>
2025-07-14 10:30:40 -0400

    wip

--- windpark/src/lib.rs
+++ windpark/src/lib.rs
@@ -1,9 +1,19 @@
+use std::f32;
+
 use wasm_bindgen::prelude::*;
 use num_dual::*;
-use nalgebra::{SVector, Vector2};
+use nalgebra::{Rotation2, SVector, Vector2};
 
 // x, y, theta, a, vx, vy, wx, wy
 const NUM_STATES: usize = 8;
+
+const WATER_DENSITY: f32 = 1000.0; // kg/m^3, typical value for water density
+const KEEL_AREA: f32 = 0.08; // m^2, area of the keel
+
+const LIFT_COEFFICIENT: f32 = 1.0;
+const DRAG_COEFFICENT: f32 = 0.7;
+
+const BOAT_MASS: f32 = 300.0; // mass of boat in kg
 
 type Float = f32;
 
@@ -42,7 +52,7 @@ impl Windpark {
     }
 }
 
-fn state_transition<D: DualNum<f32> + Copy>(input: SVector<D, 11>) -> SVector<D, NUM_STATES> {
+fn state_transition<D: DualNum<f32> + Copy + nalgebra::RealField>(input: SVector<D, 11>) -> SVector<D, NUM_STATES> {
     let dt = &input[0];
     let rudder = &input[1];
     let throttle = &input[2];
@@ -53,19 +63,40 @@ fn state_transition<D: DualNum<f32> + Copy>(input: SVector<D, 11>) -> SVector<D,
     let mut velocity = Vector2::new(input[7].clone(), input[8].clone());
     let wind_velocity = Vector2::new(input[9].clone(), input[10].clone());
 
-
     let boat_direction = Vector2::new(theta.cos(), theta.sin());
 
     let mut force = Vector2::<D>::new(D::zero(), D::zero());
 
-    // scale the boat direction by the throttle
-    let engine_force = boat_direction * (throttle.clone() * rudder.sin());
-    force += engine_force;
+    // add engine force
+    let rudder_rotation = Rotation2::new(rudder.clone());
+    force += (rudder_rotation * boat_direction.clone()) * throttle.clone();
 
-    boat_direction.dot(&wind_velocity);
-    
+    // calculate lift and drag forces from keel
+    let dynamic_pressure = velocity.norm_squared() * 0.5 * WATER_DENSITY * KEEL_AREA;
+    let angle_of_attack = boat_direction.angle(&velocity);
 
-    todo!()
+    let drag = dynamic_pressure * ((D::one() - (angle_of_attack * 2.0).cos()) * DRAG_COEFFICENT + D::one());
+    let lift = dynamic_pressure * (angle_of_attack * 2.0).sin() * LIFT_COEFFICIENT;
+
+    force += boat_direction * -D::one() * drag;
+
+    let rotate90 = Rotation2::new(D::from(f32::consts::FRAC_PI_2));
+    force += (rotate90 * boat_direction.clone()) * lift;
+
+    velocity += force * (dt.clone() / BOAT_MASS);
+    pos += velocity * dt.clone();
+
+    SVector::from([
+        pos[0],
+        pos[1],
+        theta,
+        angular_velocity,
+        velocity[0],
+        velocity[1],
+        wind_velocity[0],
+        wind_velocity[1],
+    ])
+
 }
 
 
